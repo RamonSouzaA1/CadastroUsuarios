01a4e89ca94203a38a94859efe2b08bf9dd1c43f
Almeida, Ramon de Souza(TR Technology & Ops) <Ramon.deSouzaAlmeida@thomsonreuters.com>
2021-01-28 09:18:33 -0300

    [Changes] Add repository project Add repository interface Add user repository class

---
+++ CadastroUsuarios.Repository/IRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroUsuarios.Repositorio
+{
+    public interface IRepositorio<TTipo>
+    {
+        void Adicionar(TTipo objeto);
+        void Alterar(TTipo objeto);
+        List<TTipo> Listar();
+        void Excluir(TTipo objeto);
+    }
+}
---
+++ CadastroUsuarios.Repository/UsuarioRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using CadastroUsuarios.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroUsuarios.Repositorio
+{
+    public class UsuarioRepositorio : IRepositorio<Usuario>
+    {
+        public void Adicionar(Usuario objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar(Usuario objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Usuario objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

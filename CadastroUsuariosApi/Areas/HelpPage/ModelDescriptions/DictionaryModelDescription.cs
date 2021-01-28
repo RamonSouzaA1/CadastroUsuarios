1f49871d0b174357eb2a6fa46dc2ed082f325d9a
Almeida, Ramon de Souza(TR Technology & Ops) <Ramon.deSouzaAlmeida@thomsonreuters.com>
2021-01-28 08:42:54 -0300

    Add project files.

---
+++ CadastroUsuariosApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CadastroUsuariosApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CadastroUsuariosApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace CadastroUsuariosApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
